fed00571388cc8e622dc08077e1ce951ce884af8
Filip Andersson <andersson1_filip@hotmail.com>
2025-10-03 17:37:38 +0200

    Skapade en ny klass UserManager.cs

---
+++ UserManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Kodanalys
+{
+    class UserManager
+    {
+        private List<string> users = new List<string>();
+        private const int MaxUsers = 10;
+
+        public bool AddUser(string name)
+        {
+            if (users.Count >= MaxUsers)
+                return false;
+
+            users.Add(name);
+            return true;
+        }
+
+        public List<string> GetUsers()
+        {
+            return new List<string>(users);
+        }
+
+        public bool RemoveUser(string name)
+        {
+            return users.Remove(name);
+        }
+
+        public bool UserExists(string name)
+        {
+            return users.Contains(name);
+        }
+    }
+}
